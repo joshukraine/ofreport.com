0cffa678492263807d3b2ca81c494087bf5aee8d
Joshua Steele <joshukraine@gmail.com>
2019-10-16 16:44:21 +0300

    Remove default content.

--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,10 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        ofreport.com
-      </h1>
-      <h2 class="subtitle">
-        Our family blog
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <h1>Home Page</h1>
+    <p>This is the home page.</p>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-
-export default {
-  components: {
-    Logo,
-  },
-};
+export default {};
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  margin: 0 auto;
-  min-height: 100vh;
-  text-align: center;
-}
-
-.title {
-  color: #35495e;
-  display: block;
-  font-family: Quicksand, 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 100px;
-  font-weight: 300;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  color: #526488;
-  font-size: 42px;
-  font-weight: 300;
-  padding-bottom: 15px;
-  word-spacing: 5px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
