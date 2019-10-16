f58c64a7fdde830aa388b559ac01d27254ea6bf2
Joshua Steele <joshukraine@gmail.com>
2019-10-16 16:53:49 +0300

    Add page stubs.

---
+++ pages/contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello!</h1>
+    <p>Need to get in touch? Drop us a line!</p>
+    <nuxt-link to="/">
+      Home
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/donate.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>How to Donate</h1>
+    <p>If you would like to make a financial contribution, please choose from one of the options below.</p>
+    <nuxt-link to="/">
+      Home
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
