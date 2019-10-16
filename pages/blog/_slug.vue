fca5b1ca0b1961b29c0490f37ac8f7a6178644d9
Joshua Steele <joshukraine@gmail.com>
2019-10-16 16:56:22 +0300

    Add blog post stub.

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Blog Post</h1>
+    <p>This is a blog post page.</p>
+    <nuxt-link to="/">
+      Home
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
