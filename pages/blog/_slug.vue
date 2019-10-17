8f82a81b7d37a6197cdd9d500b7bb4c713597b05
Joshua Steele <joshukraine@gmail.com>
2019-10-17 13:11:50 +0300

    Display basic Markdown content.

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -5,9 +5,24 @@
     <nuxt-link to="/">
       Home
     </nuxt-link>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <div class="content" v-html="article.html" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ params }) {
+    try {
+      const article = await import(`~/content/articles/${params.slug}.md`);
+
+      return {
+        article,
+      };
+    } catch (error) {
+      console.debug(error);
+      return false;
+    }
+  },
+};
 </script>
