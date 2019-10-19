a33db8b033c2ab29ac2635606187a0c96efcb3ee
Joshua Steele <joshukraine@gmail.com>
2019-10-19 04:07:50 +0300

    Render dynamic Markdown with Vue components.

---
+++ components/DynamicMarkdown.vue
@@ -0,0 +1,29 @@
+<script>
+import LocalComponent from '~/components/LocalComponent.vue';
+
+export default {
+  components: {
+    LocalComponent,
+  },
+  props: {
+    renderFn: {
+      type: String,
+      required: true,
+    },
+    staticRenderFns: {
+      type: String,
+      required: true,
+    },
+  },
+  created() {
+    /* eslint-disable no-new-func */
+    this.templateRender = new Function(this.renderFn)();
+    this.$options.staticRenderFns = new Function(this.staticRenderFns)();
+    /* eslint-enable no-new-func */
+  },
+
+  render(createElement) {
+    return this.templateRender ? this.templateRender() : createElement('div', 'Rendering');
+  },
+};
+</script>
