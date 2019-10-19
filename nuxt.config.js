fc0c839c77dc721e53a9e3554df805afc0a954b7
Joshua Steele <joshukraine@gmail.com>
2019-10-19 14:06:11 +0300

    Add Cloudinary plugin.
    * https://cloudinary.com/documentation/vue_integration
    * https://www.npmjs.com/package/cloudinary-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/cloudinary',
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ plugins/cloudinary.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Cloudinary from 'cloudinary-vue';
+
+Vue.use(Cloudinary, {
+  configuration: {
+    cloudName: 'dnkvsijzu',
+    components: ['CldImage'],
+  },
+});
