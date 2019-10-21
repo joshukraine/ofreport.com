31b28ef0f3971e9abab196b469b3a140a92e2e42
Joshua Steele <joshukraine@gmail.com>
2019-10-21 12:19:33 +0300

    Add page/_id.vue

---
+++ pages/tags/_tag/page/_id.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Articles tagged with: {{ tag }}</h1>
+    <ArticlesList :all-articles="taggedArticles"
+                  :start-page="startPage"
+                  :root-segment="rootSegment"
+    />
+  </div>
+</template>
+
+<script>
+import ArticlesList from '~/components/ArticlesList.vue';
+import articles from '~/data/articles.json';
+
+const stringParameterize = (tag) => tag.trim()
+  .toLowerCase().replace(/[^a-zA-Z0-9 -]/, '').replace(/\s/g, '-');
+
+export default {
+  components: {
+    ArticlesList,
+  },
+  validate({ params }) {
+    return /^\d+$/.test(params.id);
+  },
+  asyncData({ params }) {
+    const startPage = parseInt(params.id);
+    const rootSegment = `tags/${params.tag}`;
+    const taggedArticles = Object.values(articles).reverse()
+      .filter((article) => {
+        const safeTags = article.tags.map((tag) => stringParameterize(tag));
+        return safeTags.includes(params.tag);
+      })
+      .map((article) => article);
+
+    return {
+      taggedArticles,
+      tag: params.tag,
+      rootSegment,
+      startPage,
+    };
+  },
+};
+</script>
