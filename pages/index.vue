55e892f3752df30c4f39e9b899cac86a214cf0db
Joshua Steele <joshukraine@gmail.com>
2019-10-21 11:55:13 +0300

    Rename variables.
    * articles -> allArticles
    * data -> articles

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <ArticlesList :all-articles="articles" :start-page="1" />
+    <ArticlesList :all-articles="allArticles" :start-page="1" />
   </div>
 </template>
 
 <script>
 import ArticlesList from '~/components/ArticlesList.vue';
-import data from '~/data/articles.json';
+import articles from '~/data/articles.json';
 
 export default {
   components: {
     ArticlesList,
   },
   asyncData() {
-    const articles = Object.values(data).reverse();
+    const allArticles = Object.values(articles).reverse();
 
     return {
-      articles,
+      allArticles,
     };
   },
 };
--- pages/page/_id.vue
+++ pages/page/_id.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <ArticlesList :all-articles="articles" :start-page="startPage" />
+    <ArticlesList :all-articles="allArticles" :start-page="startPage" />
   </div>
 </template>
 
 <script>
 import ArticlesList from '~/components/ArticlesList.vue';
-import data from '~/data/articles.json';
+import articles from '~/data/articles.json';
 
 export default {
   middleware: 'index-redirect',
@@ -18,11 +18,11 @@ export default {
     return /^\d+$/.test(params.id);
   },
   asyncData({ params }) {
-    const articles = Object.values(data).reverse();
+    const allArticles = Object.values(articles).reverse();
     const startPage = parseInt(params.id);
 
     return {
-      articles,
+      allArticles,
       startPage,
     };
   },
