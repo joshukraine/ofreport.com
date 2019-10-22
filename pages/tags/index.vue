eae7e49256c9828f471a953c9cbe81e96eea5f67
Joshua Steele <joshukraine@gmail.com>
2019-10-22 14:44:49 +0300

    Add tags index.

---
+++ pages/tags/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>All Tags</h1>
+    <ul>
+      <li v-for="tag in tags" :key="tag">
+        <nuxt-link :to="`/tags/${tag}`">
+          {{ tag }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import articles from '~/data/articles.json';
+import { getUniqueTags } from '~/lib/helpers';
+
+export default {
+  data() {
+    return {
+      tags: [],
+    };
+  },
+  created() {
+    this.tags = getUniqueTags(articles);
+  },
+};
+</script>
