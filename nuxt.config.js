98cfd7880a4a0aa255e518589e4f24cd5d33b02f
Joshua Steele <joshukraine@gmail.com>
2019-10-22 12:32:13 +0300

    Generate nested pagination routes.

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,10 +20,39 @@ Object.values(articles).map((a) => a.tags
   .map((tag) => rawTags.push(parameterize(tag))));
 const uniqueTags = [...new Set(rawTags)];
 
+// Function for retrieving articles which match a give tag
+const taggedArticlesFor = (tag) => Object.values(articles)
+  .filter((article) => {
+    const safeTags = article.tags.map((t) => parameterize(t));
+    return safeTags.includes(tag);
+  })
+  .map((article) => article);
+
+// deepTagArray returns a nested array of path groups.
+// Example:
+// [
+//   [ '/tags/cmo/page/1', '/tags/cmo/page/2', '/tags/cmo/page/3' ],
+//   [ '/tags/video/page/1', '/tags/video/page/2', '/tags/video/page/3' ],
+//   [ '/tags/family/page/1', '/tags/family/page/2', '/tags/family/page/3' ],
+// ]
+const deepTagArray = uniqueTags.map((tag) => {
+  const localArticles = taggedArticlesFor(tag);
+  const localArticleCount = localArticles.length;
+  const tagPages = [...Array(Math.ceil(localArticleCount / perPage))];
+
+  const tagPathGroup = [];
+  tagPages.map((_, i) => tagPathGroup.push(`/tags/${tag}/page/${i + 1}`));
+
+  // tagPathGroup is an array of paths.
+  // Example: [ '/tags/cmo/page/1', '/tags/cmo/page/2', '/tags/cmo/page/3' ]
+  return tagPathGroup;
+});
+
 const dynamicRoutes = () => [].concat(
   articleSlugs.map((slug) => `/blog/${slug}`),
   articlePages.map((_, i) => `/page/${i + 1}`),
   uniqueTags.map((tag) => `/tags/${tag}`),
+  [].concat(...deepTagArray),
 );
 
 export default {
