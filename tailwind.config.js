d87dcc265e705e012d6fcb6e6c2f4f90723abaaa
Joshua Steele <joshukraine@gmail.com>
2019-10-22 16:47:45 +0300

    Add blue and gray colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,36 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          '050': '#E3F8FF',
+          100: '#B3ECFF',
+          200: '#81DEFD',
+          300: '#5ED0FA',
+          400: '#40C3F7',
+          500: '#2BB0ED',
+          600: '#1992D4',
+          700: '#127FBF',
+          800: '#0B69A3',
+          900: '#035388',
+        },
+        gray: {
+          '050': '#F5F7FA',
+          100: '#E4E7EB',
+          200: '#CBD2D9',
+          300: '#9AA5B1',
+          400: '#7B8794',
+          500: '#616E7C',
+          600: '#52606D',
+          700: '#3E4C59',
+          800: '#323F4B',
+          900: '#1F2933',
+        },
+      },
+    },
+  },
   variants: {},
   plugins: [],
 };
