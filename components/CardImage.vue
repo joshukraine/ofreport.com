9b2618fd14d20321dbe58bbb5910da280b521c00
Joshua Steele <joshukraine@gmail.com>
2019-10-24 01:29:54 +0300

    Add CardImage component.

---
+++ components/CardImage.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable vue/attribute-hyphenation -->
+<template>
+  <figure class="mt-4">
+    <cld-image :public-id="`OFReport/${publicId}`">
+      <cld-transformation :width="width"
+                          :alt="alt"
+                          crop="scale"
+                          fetchFormat="auto"
+                          quality="auto:best"
+      />
+    </cld-image>
+  </figure>
+</template>
+
+<script>
+export default {
+  props: {
+    articleCover: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    publicId() {
+      return this.articleCover.split('OFReport/')[1];
+    },
+  },
+};
+</script>
