f8ba95998ecab9d239e0a601a36424e39a226a0c
Joshua Steele <joshukraine@gmail.com>
2019-10-23 17:12:17 +0300

    Add background gradient while image loads.
    * https://www.cssmatic.com/gradient-generator#'\-moz\-linear\-gradient\%28top\%2C\%20\%231f415c\%200\%25\%2C\%20\%230f2847\%20100\%25\%29\%3B'

--- components/Alpha.vue
+++ components/Alpha.vue
@@ -1,22 +1,24 @@
 <template>
-  <section class="alpha bg-cover bg-top sm:bg-left-top flex items-center md:bg-left lg:bg-top">
-    <div class="container pb-10 sm:pb-0 md:py-32">
-      <div class="flex justify-end pt-24 sm:pt-32 md:pt-0 md:px-8 lg:px-16">
-        <div class="text-center px-8 md:w-2/5 md:px-0">
-          <img class="w-full" src="~assets/svg/josh-and-kels.svg">
-          <p class="text-white font-header mb-8 md:mb-12">
-            Est. 2004
-          </p>
-          <p class="text-white font-header mb-8 sm:text-xl md:mb-12">
-            We live in Ukraine where we strive to tell others about God's gift of eternal life through Jesus Christ.
-          </p>
-          <nuxt-link to="/family" class="btn btn-lg btn-blue">
-            Meet our Family
-          </nuxt-link>
+  <div style="background: linear-gradient(to bottom, #1f415c 0%, #0f2847 100%);">
+    <section class="alpha bg-cover bg-top sm:bg-left-top flex items-center md:bg-left lg:bg-top">
+      <div class="container pb-10 sm:pb-0 md:py-32">
+        <div class="flex justify-end pt-24 sm:pt-32 md:pt-0 md:px-8 lg:px-16">
+          <div class="text-center px-8 md:w-2/5 md:px-0">
+            <img class="w-full" src="~assets/svg/josh-and-kels.svg">
+            <p class="text-white font-header mb-8 md:mb-12">
+              Est. 2004
+            </p>
+            <p class="text-white font-header mb-8 sm:text-xl md:mb-12">
+              We live in Ukraine where we strive to tell others about God's gift of eternal life through Jesus Christ.
+            </p>
+            <nuxt-link to="/family" class="btn btn-lg btn-blue">
+              Meet our Family
+            </nuxt-link>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <style scoped>
