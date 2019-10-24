c6471098b3f07fc34519502cf15c49a22d422864
Joshua Steele <joshukraine@gmail.com>
2019-10-24 23:30:12 +0300

    Style preview cards with cover image on top.

--- components/ArticlePreview.vue
+++ components/ArticlePreview.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="md:mx-4 p-4 h-full flex flex-col justify-between bg-white rounded-lg shadow-md">
+  <div class="md:mx-4 p-4 h-full flex flex-col justify-between bg-white rounded-lg overflow-hidden shadow-md">
     <div>
-      <h2 class="my-0 leading-none">
-        {{ article.title }}
-      </h2>
-      <p class="mt-1 text-sm text-gray-400">
-        <span>{{ article.author }}</span>
-        <span>&middot; {{ article.date }}</span>
-      </p>
-      <div v-if="article.cover" class="-mx-4">
+      <div v-if="article.cover" class="-mx-4 -mt-4">
         <card-image :article-cover="article.cover"
                     :width="customWidth"
                     :alt="article.caption"
         />
       </div>
-      <p class="text-base">
-        {{ article.preview }}
-      </p>
+      <div>
+        <h2 class="mt-4 leading-none">
+          {{ article.title }}
+        </h2>
+        <p class="mt-1 text-sm text-gray-400">
+          <span>{{ article.author }}</span>
+          <span>&middot; {{ article.date }}</span>
+        </p>
+        <p class="text-base">
+          {{ article.preview }}
+        </p>
+      </div>
     </div>
     <p class="border-t pt-2 mt-6">
       <nuxt-link class="text-base font-bold" :to="`/blog/${article.basename}`">
