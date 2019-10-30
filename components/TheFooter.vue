d76a08795091a6e46c840b03728e635d1bbacbf8
Joshua Steele <joshukraine@gmail.com>
2019-10-30 19:15:58 +0200

    Add footer component.

---
+++ components/TheFooter.vue
@@ -0,0 +1,136 @@
+<template>
+  <footer class="bg-blue-900">
+    <div class="container py-6 max-w-4xl">
+      <p class="mt-0 text-center text-blue-footer font-fancy text-xl md:text-2xl leading-snug tracking-wide">
+        But as we were allowed of God to be put in trust with the gospel,
+        even so we speak; not as pleasing men, but God, which trieth our
+        hearts. <span class="whitespace-no-wrap">1 Thessalonians 2:4</span>
+      </p>
+    </div>
+    <div class="border-t border-blue-footer">
+      <div class="container pt-6 sm:pt-8 flex mx-auto max-w-lg">
+        <ul class="w-1/3 text-center">
+          <li>
+            <nuxt-link to="/family/">
+              Family
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/ministry/">
+              Ministry
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/blog/">
+              Blog
+            </nuxt-link>
+          </li>
+        </ul>
+        <ul class="w-1/3 text-center">
+          <li>
+            <nuxt-link to="/archives/">
+              Archives
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/contact/">
+              Contact
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/donate/">
+              Donate
+            </nuxt-link>
+          </li>
+        </ul>
+        <ul class="w-1/3 text-center">
+          <li>
+            <a href="https://euroteamoutreach.org/" target="_blank">ETO</a>
+          </li>
+          <li>
+            <a href="https://getbiblefirst.com/" target="_blank">Bible First</a>
+          </li>
+          <li>
+            <a href="https://cmoproject.org/" target="_blank">CMO</a>
+          </li>
+        </ul>
+      </div>
+      <ul class="py-6 sm:py-8 flex justify-center">
+        <li class="mx-6">
+          <a href="https://www.facebook.com/joshukraine"
+             class=""
+             title="Joshua on Facebook"
+             target="_blank"
+          >
+            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+              <path d="M504 248C504 111 393 0 256 0S8 111 8 248c0 123.78 90.69 226.38 209.25 245V319.69h-63V248h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V248h68.78l-11 71.69h-57.78V493C413.31 474.38 504 371.78 504 248z" />
+            </svg>
+          </a>
+        </li>
+        <li class="mx-6">
+          <a href="https://twitter.com/joshukraine"
+             class=""
+             title="Joshua on Twitter"
+             target="_blank"
+          >
+            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M256 496c136.967 0 248-111.033 248-248C504 111.033 392.967 0 256 0 119.033 0 8 111.033 8 248c0 136.967 111.033 248 248 248zm132.983-306.852c0-2.953-.001-5.906-.212-8.859 13.312-9.704 24.934-21.727 34.229-35.226-12.255 5.273-25.566 9.07-39.301 10.546 14.157-8.437 24.933-21.726 30.004-37.546-13.101 7.804-27.891 13.5-43.315 16.453C357.921 121.227 340.172 113 320.522 113c-37.822 0-68.248 30.586-68.248 68.133 0 5.273.634 10.546 1.69 15.609-56.626-2.954-107.126-29.953-140.722-71.297-5.917 10.125-9.297 21.727-9.297 34.383 0 23.625 12.043 44.507 30.426 56.742a68.813 68.813 0 0 1-30.849-8.648v.844c0 33.117 23.455 60.538 54.726 66.867-5.704 1.476-11.832 2.32-17.96 2.32-4.437 0-8.663-.422-12.889-1.055 8.663 27.001 33.807 46.618 63.811 47.251-23.454 18.351-52.824 29.109-84.729 29.109-5.705 0-10.987-.211-16.481-.844C120.215 371.82 156.135 383 194.802 383c125.51 0 194.181-103.782 194.181-193.852z"
+              />
+            </svg>
+          </a>
+        </li>
+        <li class="mx-6">
+          <a href="https://github.com/joshukraine/ofreport.com"
+             class=""
+             title="View source on GitHub"
+             target="_blank"
+          >
+            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+              <path d="M252.8 0C114.1 0 8 105.3 8 244c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5 21.3 0 42.8 2.9 62.8 8.5 0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C436.2 449.8 504 354.9 504 244 504 105.3 391.5 0 252.8 0z" />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="border-t border-blue-footer">
+      <div class="container py-6 sm:py-8">
+        <p class="mt-0 text-center text-blue-footer">
+          © <span class="year">2019</span> Joshua and Kelsie Steele
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer a {
+  @apply .text-sm .text-blue-footer;
+}
+
+footer a:hover {
+  @apply .text-blue-050;
+}
+
+@screen xs {
+  footer a {
+    @apply .text-base;
+  }
+}
+
+@screen sm {
+  footer a {
+    @apply .text-lg;
+  }
+}
+
+footer svg {
+  @apply .fill-current .h-8 .w-8 .text-blue-footer;
+  transition: color 150ms ease-in-out;
+}
+
+footer svg:hover {
+  @apply .text-blue-050;
+}
+</style>
