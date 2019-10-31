65b583993482558d6b90a14ed424aec2ad719df2
Joshua Steele <joshukraine@gmail.com>
2019-10-31 21:43:04 +0200

    Allow 3 attrs on single line.

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,11 +1,7 @@
 <template>
   <section class="my-6 sm:my-8 lg:my-12 text-center">
-    <!-- eslint-disable vue/no-v-html -->
-    <h1 v-if="html"
-        class="lg:text-6xl"
-        v-html="title"
-    />
-    <!-- eslint-enable vue/no-v-html -->
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <h1 v-if="html" class="lg:text-6xl" v-html="title" />
     <h1 v-else class="lg:text-6xl">
       {{ title }}
     </h1>
