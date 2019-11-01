93ad1db0c1075d6f77587bc366c0a7e8bd1ed427
Joshua Steele <joshukraine@gmail.com>
2019-11-01 10:39:53 +0200

    Add donate page.

---
+++ content/pages/donate.md
@@ -0,0 +1,16 @@
+If you would like to make a financial contribution, please choose from one of the options below. We are grateful for your kindness and generosity!
+
+## Option 1 - Donate online
+
+Click the button below to make a donation online via PayPal.
+
+<article-button text="Donate Online" path="https://www.paypal.me/joshukraine" :outline="true" :external="true" margin="t" />
+
+## Option 2 - Donate by mail
+
+Send a check, payable to **Fairpark Baptist Church**, to the following address:
+
+> **Fairpark Baptist Church  
+> ATTN: Steele Family Missions  
+> 6000 Crowley Rd.  
+> Fort Worth, TX 76134**
--- pages/donate.vue
+++ pages/donate.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <h1>How to Donate</h1>
-    <p>If you would like to make a financial contribution, please choose from one of the options below.</p>
-    <nuxt-link to="/">
-      Home
-    </nuxt-link>
-  </div>
+  <article class="mb-8 md:mb-12">
+    <PageHeader title="How to Donate" />
+
+    <section class="container max-w-2xl mx-auto mt-6 sm:mt-8 md:mt-10 lg:mt-12">
+      <DynamicMarkdown
+        :render-fn="renderFn"
+        :static-render-fns="staticRenderFns"
+      />
+    </section>
+  </article>
 </template>
 
 <script>
-export default {};
+import DynamicMarkdown from '~/components/DynamicMarkdown.vue';
+import PageHeader from '~/components/PageHeader.vue';
+
+export default {
+  components: {
+    DynamicMarkdown,
+    PageHeader,
+  },
+  async asyncData() {
+    const page = await import('~/content/pages/donate.md');
+    return {
+      renderFn: page.vue.render,
+      staticRenderFns: page.vue.staticRenderFns,
+    };
+  },
+};
 </script>
