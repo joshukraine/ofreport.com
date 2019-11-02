945254acc2ecdea28525c0604ea0f8ed83bca766
Joshua Steele <joshukraine@gmail.com>
2019-11-02 20:35:08 +0200

    Add basic post functionality.

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,11 +8,12 @@
       </p>
 
       <div class="mt-6 p-4 bg-white rounded shadow-md">
-        <form>
+        <form @submit.prevent="postMessage">
           <label class="form-label" for="name">
             Your Name
           </label>
-          <input maxlength="100"
+          <input v-model="name"
+                 maxlength="100"
                  name="name"
                  placeholder="Jane Doe"
                  type="text"
@@ -22,7 +23,8 @@
           <label class="form-label" for="email">
             Your Email
           </label>
-          <input maxlength="100"
+          <input v-model="email"
+                 maxlength="100"
                  name="email"
                  placeholder="you@example.com"
                  type="email"
@@ -31,7 +33,8 @@
           <label class="form-label" for="message">
             Your Message
           </label>
-          <textarea cols="30"
+          <textarea v-model="message"
+                    cols="30"
                     rows="10"
                     maxlength="3000"
                     name="message"
@@ -41,6 +44,9 @@
           <input name="_gotcha" style="display: none;" type="text">
           <input type="submit" value="Send" class="btn btn-blue btn-lg block mt-6 w-full sm:w-auto">
         </form>
+        <p v-if="status" class="text-right">
+          {{ status }}
+        </p>
       </div>
     </section>
   </article>
@@ -52,6 +58,34 @@ import PageHeader from '~/components/PageHeader.vue';
 export default {
   components: {
     PageHeader,
+  },
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      status: '',
+    };
+  },
+  methods: {
+    async postMessage() {
+      try {
+        await this.$axios.$post('/email/send/json', {
+          name: this.name,
+          email: this.email,
+          content: this.message,
+        });
+        this.status = 'Message sent!';
+        this.formReset();
+      } catch (error) {
+        this.status = 'Oops, there was an error!';
+      }
+    },
+    formReset() {
+      this.name = '';
+      this.email = '';
+      this.message = '';
+    },
   },
 };
 </script>
