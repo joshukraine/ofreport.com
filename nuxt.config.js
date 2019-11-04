3b20c6e5e42d684a75e9566c3a5af84ac9dc82c8
Joshua Steele <joshukraine@gmail.com>
2019-11-04 20:47:01 +0200

    Add dynamic title and description.

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,11 +14,14 @@ export default {
   },
 
   head: {
-    title: process.env.npm_package_name || '',
+    titleTemplate: (titleChunk) => {
+      const baseTitle = 'Joshua and Kelsie Steele — Missionaries serving Christ in Ukraine';
+      return titleChunk ? `${titleChunk} | ${baseTitle}` : baseTitle;
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'Joshua and Kelsie are missionaries enjoying life as best friends, serving their Savior, and raising up their children to honor Him.' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
