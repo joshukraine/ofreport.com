fa0f56a1e1a5fafa843683d46326e94ca50b2c90
Joshua Steele <joshukraine@gmail.com>
2019-11-06 00:24:23 +0200

    Add "view all tags" link.

--- components/TagHome.vue
+++ components/TagHome.vue
@@ -3,6 +3,11 @@
     <section class="container">
       <div class="my-6 sm:my-8 md:mb-0 lg:mt-12 text-center">
         <h1>Articles tagged "{{ tag }}"</h1>
+        <p>
+          <nuxt-link to="/tags">
+            View all tags
+          </nuxt-link>
+        </p>
       </div>
     </section>
     <ArticlesList :all-articles="taggedArticles"
