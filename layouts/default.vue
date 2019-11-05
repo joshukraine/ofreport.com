0f1549eb5183f48a7502b07b885a47e42a26dfad
Joshua Steele <joshukraine@gmail.com>
2019-11-05 09:24:03 +0200

    Add OG tags.

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,8 +11,24 @@
 <script>
 import TheNavbar from '~/components/TheNavbar';
 import TheFooter from '~/components/TheFooter.vue';
+import site from '~/data/site.json';
 
 export default {
+  head() {
+    return {
+      meta: [
+        { hid: 'og:url', property: 'og:url', content: site.url + this.$route.path },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:title', property: 'og:title', content: site.title },
+        { hid: 'og:description', property: 'og:description', content: site.description },
+        { hid: 'og:image', property: 'og:image', content: site.image },
+        { hid: 'og:image:width', property: 'og:image:width', content: '1200' },
+        { hid: 'og:image:height', property: 'og:image:height', content: '630' },
+        { property: 'og:site_name', content: site.name },
+        { property: 'og:locale', content: 'en_US' },
+      ],
+    };
+  },
   components: {
     TheNavbar,
     TheFooter,
