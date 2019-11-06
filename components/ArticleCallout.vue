64c3a6ef76a01d4ec92077a8cb3b3cf92337d4ce
Joshua Steele <joshukraine@gmail.com>
2019-11-06 08:49:53 +0200

    Add ArticleCallout component.
    * This will hand simple text or PDF newsletter links.

---
+++ components/ArticleCallout.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="py-4 sm:py-6 border-t-4 border-b-4" :class="marginClasses">
+    <p v-if="download" class="pdf-download m-0 sm:text-xl font-bold text-center">
+      <a class="inline-block"
+         :href="downloadLink"
+         target="_blank"
+         rel="noopener noreferrer"
+      >
+        Download PDF Newsletter
+      </a>
+    </p>
+
+    <p v-else class="m-0 sm:text-xl font-bold text-center">
+      {{ content }}
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+    download: {
+      type: Boolean,
+      default: false,
+    },
+    mt: {
+      type: String,
+      default: '10',
+    },
+    mb: {
+      type: String,
+      default: '10',
+    },
+  },
+  computed: {
+    downloadLink() {
+      const cdn = 'https://d21yo20tm8bmc2.cloudfront.net/ofr/';
+      return cdn + this.content;
+    },
+    marginClasses() {
+      return `mt-${this.mt} mb-${this.mb}`;
+    },
+  },
+};
+</script>
+
+<style>
+.pdf-download > a:before {
+  background-image: url("data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgNjQwIDUxMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cGF0aCBkPSJNNTM3LjYgMjI2LjZjNC4xLTEwLjcgNi40LTIyLjQgNi40LTM0LjYgMC01My00My05Ni05Ni05Ni0xOS43IDAtMzguMSA2LTUzLjMgMTYuMkMzNjcgNjQuMiAzMTUuMyAzMiAyNTYgMzJjLTg4LjQgMC0xNjAgNzEuNi0xNjAgMTYwIDAgMi43LjEgNS40LjIgOC4xQzQwLjIgMjE5LjggMCAyNzMuMiAwIDMzNmMwIDc5LjUgNjQuNSAxNDQgMTQ0IDE0NGgzNjhjNzAuNyAwIDEyOC01Ny4zIDEyOC0xMjggMC02MS45LTQ0LTExMy42LTEwMi40LTEyNS40em0tMTMyLjkgODguN0wyOTkuMyA0MjAuN2MtNi4yIDYuMi0xNi40IDYuMi0yMi42IDBMMTcxLjMgMzE1LjNjLTEwLjEtMTAuMS0yLjktMjcuMyAxMS4zLTI3LjNIMjQ4VjE3NmMwLTguOCA3LjItMTYgMTYtMTZoNDhjOC44IDAgMTYgNy4yIDE2IDE2djExMmg2NS40YzE0LjIgMCAyMS40IDE3LjIgMTEuMyAyNy4zeiIgZmlsbD0iI0U1M0UzRSIvPgo8L3N2Zz4K");
+  background-position: center;
+  background-repeat: no-repeat;
+  content: "";
+  display: block;
+  float: left;
+  height: 1.3em;
+  margin-left: -1.5em;
+  margin-top: 1px;
+  width: 1.3em;
+}
+
+@screen sm {
+  .pdf-download > a:before {
+    margin-left: -1.7em;
+  }
+}
+</style>
