b0ff16ecbd787fbd69ef6ea5b0e5bc3a72adcd8b
Joshua Steele <joshukraine@gmail.com>
2019-11-07 16:54:45 +0200

    Extra feed config to separate module.

---
+++ config/feed.js
@@ -0,0 +1,43 @@
+import markdownIt from 'markdown-it';
+import dayjs from 'dayjs';
+import articleData from '../data/articles.json';
+import site from '../data/site.json';
+
+const md = markdownIt({
+  linkify: true,
+  typographer: true,
+});
+
+export default () => {
+  const feedConfig = {
+    path: '/feed.xml',
+    async create(feed) {
+      const cpYear = dayjs().format('YYYY');
+      const articles = Object.values(articleData).reverse();
+      const recentArticles = articles.slice(0, 10);
+
+      feed.options = {
+        title: 'OFReport.com — Joshua and Kelsie Steele',
+        description: 'Joshua and Kelsie are missionaries enjoying life as best friends, serving their Savior, and raising up their children to honor Him.',
+        id: 'https://ofreport.com/',
+        link: 'https://ofreport.com/feed.xml',
+        language: site.lang,
+        copyright: `Copyright ${cpYear}, Joshua and Kelsie Steele`,
+      };
+
+      recentArticles.forEach((a) => {
+        feed.addItem({
+          title: a.title,
+          link: `${site.url}/blog/${a.basename}`,
+          description: md.render(a.preview),
+          date: new Date(a.iso8601Date),
+          image: a.cover,
+        });
+      });
+    },
+    cacheTime: 1000 * 60 * 15,
+    type: 'rss2',
+  };
+
+  return feedConfig;
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 import markdownIt from 'markdown-it';
 import mila from 'markdown-it-link-attributes';
 import path from 'path';
-import dayjs from 'dayjs';
 import dynamicRoutes from './lib/dynamic-routes';
 import site from './data/site.json';
-import articleData from './data/articles.json';
+
+import { feed } from './config';
 
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 require('dotenv').config();
@@ -67,42 +67,7 @@ export default {
     '@nuxtjs/sitemap',
   ],
 
-  feed: [
-    {
-      path: '/feed.xml',
-      async create(feed) {
-        const cpYear = dayjs().format('YYYY');
-        const articles = Object.values(articleData).reverse();
-        const recentArticles = articles.slice(0, 10);
-
-        const md = markdownIt({
-          linkify: true,
-          typographer: true,
-        });
-
-        feed.options = {
-          title: 'OFReport.com — Joshua and Kelsie Steele',
-          description: 'Joshua and Kelsie are missionaries enjoying life as best friends, serving their Savior, and raising up their children to honor Him.',
-          id: 'https://ofreport.com/',
-          link: 'https://ofreport.com/feed.xml',
-          language: site.lang,
-          copyright: `Copyright ${cpYear}, Joshua and Kelsie Steele`,
-        };
-
-        recentArticles.forEach((a) => {
-          feed.addItem({
-            title: a.title,
-            link: `${site.url}/blog/${a.basename}`,
-            description: md.render(a.preview),
-            date: new Date(a.iso8601Date),
-            // image: a.cover,
-          });
-        });
-      },
-      cacheTime: 1000 * 60 * 15,
-      type: 'rss2',
-    },
-  ],
+  feed,
 
   recaptcha: {
     language: 'en',
