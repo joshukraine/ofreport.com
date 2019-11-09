25d8410697b5189511bf1c2c6c5a05e431894409
Joshua Steele <joshukraine@gmail.com>
2019-11-09 14:01:18 +0200

    Experiment with static ES6 import.
    * This solves the new issue in FML v3.1 of asyncData imports not rendering properly.
    * However, this approach is not viable for blog posts which need to be loaded dynamically inside asyncData.

--- pages/ministry.vue
+++ pages/ministry.vue
@@ -14,24 +14,26 @@
 <script>
 import DynamicMarkdown from '~/components/DynamicMarkdown.vue';
 import PageHeader from '~/components/PageHeader.vue';
+import ministry from '~/content/pages/ministry.md';
 
 export default {
   components: {
     DynamicMarkdown,
     PageHeader,
   },
-  async asyncData() {
-    const page = await import('~/content/pages/ministry.md');
-    return {
-      renderFn: page.vue.render,
-      staticRenderFns: page.vue.staticRenderFns,
-    };
-  },
   data() {
     return {
+      fm: null,
+      renderFn: null,
+      staticRenderFns: null,
       description: 'Our highest purpose in life is to follow our Savior, the Lord Jesus Christ. We are raising our family in Ukraine, and striving to share the Gospel with those around us. If you’d like to learn more about how we minister, read on!',
       title: 'Ministry',
     };
+  },
+  created() {
+    this.fm = ministry.attributes;
+    this.renderFn = ministry.vue.render;
+    this.staticRenderFns = ministry.vue.staticRenderFns;
   },
   head() {
     return {
