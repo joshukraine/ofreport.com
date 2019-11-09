1f10bd855171f1f9055a4e6c00345010c9e84272
Joshua Steele <joshukraine@gmail.com>
2019-11-09 16:52:56 +0200

    Update head links.

--- config/head.js
+++ config/head.js
@@ -14,9 +14,19 @@ export default {
     { hid: 'description', name: 'description', content: site.description },
     { hid: 'author', name: 'author', content: site.author },
     { hid: 'robots', name: 'robots', content: process.env.APP_ENV === 'prod' ? 'index,follow' : 'noindex,nofollow' },
+    { name: 'msapplication-TileColor', content: '#2d89ef' },
+    { name: 'theme-color', content: '#ffffff' },
   ],
   link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    {
+      rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png',
+    },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
     {
       rel: 'alternate', type: 'application/rss+xml', title: 'RSS Feed', href: 'https://ofreport.com/feed.xml',
     },
