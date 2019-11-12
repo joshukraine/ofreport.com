c1868f744264655bd8064c090f1134e2b285c767
Joshua Steele <joshukraine@gmail.com>
2019-11-12 09:47:40 +0200

    Add dynamic height classes computed prop.

--- components/ArticleSpacer.vue
+++ components/ArticleSpacer.vue
@@ -1,3 +1,23 @@
 <template>
-  <div class="h-8" />
+  <div :class="heightClasses" />
 </template>
+
+<script>
+export default {
+  props: {
+    height: {
+      type: String,
+      default: '10',
+    },
+    heightSm: {
+      type: String,
+      default: '10',
+    },
+  },
+  computed: {
+    heightClasses() {
+      return `h-${this.height} sm:h-${this.heightSm}`;
+    },
+  },
+};
+</script>
