2515550f460f5c5c3e6fbf4a275a5f339765e49c
Joshua Steele <joshukraine@gmail.com>
2019-11-18 09:48:48 +0200

    Add subscribe page stub.

---
+++ pages/subscribe.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="pb-8 md:pb-20">
+    <PageHeader title="Subscribe" />
+
+    <section class="container max-w-2xl mx-auto -mt-4">
+      <p>content here...</p>
+    </section>
+  </article>
+</template>
+
+<script>
+import PageHeader from '~/components/PageHeader.vue';
+
+export default {
+  components: {
+    PageHeader,
+  },
+};
+</script>
