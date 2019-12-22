827800f4ec46d1fe281b702abbcf919e084902b5
Joshua Steele <joshukraine@gmail.com>
2019-12-22 21:57:42 +0200

    Update ArticleSvg component with link and margin props.

--- components/ArticleSvg.vue
+++ components/ArticleSvg.vue
@@ -1,6 +1,16 @@
 <template>
-  <figure class="mb-8">
-    <img :src="require(`../assets/svg/${name}.svg`)"
+  <figure :class="verticalSpacing">
+    <a v-if="link"
+       :href="link"
+       target="_blank"
+    >
+      <img :src="require(`../assets/svg/${name}.svg`)"
+           :width="width"
+           :alt="alt"
+      >
+    </a>
+    <img v-else
+         :src="require(`../assets/svg/${name}.svg`)"
          :width="width"
          :alt="alt"
     >
@@ -21,6 +31,22 @@ export default {
     alt: {
       type: String,
       required: true,
+    },
+    margin: {
+      type: String,
+      default: '',
+    },
+    link: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    verticalSpacing() {
+      const side = this.margin;
+      const marginClasses = `m${side}-6 md:m${side}-8`;
+      const options = ['t', 'b', 'y'];
+      return options.includes(side) ? marginClasses : 'my-0';
     },
   },
 };
