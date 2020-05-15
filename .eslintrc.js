52efbf37b30a2d1d1a5be4220755b96d9a3adc82
Joshua Steele <joshukraine@gmail.com>
2020-05-15 16:43:52 +0300

    Trying out Prettier with Vue. 😰🤞🏻

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,14 @@ module.exports = {
     es6: true,
   },
   extends: [
-    // https://eslint.vuejs.org/rules/
-    'plugin:vue/recommended',
-
     // https://github.com/airbnb/javascript
     'airbnb-base',
+
+    // https://eslint.vuejs.org/rules/
+    'plugin:vue/essential',
+
+    // https://prettier.io/docs/en/integrating-with-linters.html#use-eslint-to-run-prettier
+    'plugin:prettier/recommended',
   ],
   globals: {
     Atomics: 'readonly',
@@ -22,22 +25,27 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-  ],
+  plugins: ['vue'],
   settings: {
     'import/core-modules': ['vue'],
   },
   rules: {
+    // https://prettier.io/docs/en/integrating-with-linters.html#eslint
+    'prettier/prettier': 'error',
+
     // Allow unresolved imports
     // https://github.com/nuxt/eslint-config/blob/master/packages/eslint-config/index.js
     'import/no-unresolved': 'off',
 
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-    'import/extensions': ['error', 'never', {
-      vue: 'always',
-      json: 'always',
-    }],
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        vue: 'always',
+        json: 'always',
+      },
+    ],
 
     // https://eslint.org/docs/rules/max-len
     // https://github.com/vuejs/eslint-plugin-vue/issues/731
@@ -54,15 +62,22 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
 
     // https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/max-attributes-per-line.md
-    'vue/max-attributes-per-line': ['error', {
-      singleline: 2,
-      multiline: {
-        max: 1,
-        allowFirstLine: true,
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 2,
+        multiline: {
+          max: 1,
+          allowFirstLine: true,
+        },
       },
-    }],
-    'vue/attribute-hyphenation': ['error', 'always', {
-      ignore: ['fetchFormat'],
-    }],
+    ],
+    'vue/attribute-hyphenation': [
+      'error',
+      'always',
+      {
+        ignore: ['fetchFormat'],
+      },
+    ],
   },
 };
