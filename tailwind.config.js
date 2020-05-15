5cb38fd6c312b26b0d8e44662354422375b24175
Joshua Steele <joshukraine@gmail.com>
2020-05-15 22:31:25 +0300

    Reintroduce whitelist and whitelistPatterns.
    * I have some dynamic classes that get removed without this.

--- tailwind.config.js
+++ tailwind.config.js
@@ -115,5 +115,19 @@ module.exports = {
       'plugins/**/*.js',
       'nuxt.config.js',
     ],
+    options: {
+      whitelist: ['blockquote', 'ul', 'ol', 'li', 'markdown'],
+      whitelistPatterns: [
+        /^fade/,
+        /^mt-/,
+        /^mb-/,
+        /^my-/,
+        /^md:mt-/,
+        /^md:mb-/,
+        /^md:my-/,
+        /^h-/,
+        /^sm:h-/,
+      ],
+    },
   },
 };
