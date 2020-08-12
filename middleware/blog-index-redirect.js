714bf19917c3b22a3572b3108aadd6767e22744e
Joshua Steele <joshukraine@gmail.com>
2020-08-12 09:49:52 +0300

    Use arrow functions for middleware.

--- middleware/blog-index-redirect.js
+++ middleware/blog-index-redirect.js
@@ -1,6 +1,8 @@
-export default function ({ params, redirect }) {
+const blogIndexRedirect = ({ params, redirect }) => {
   if (parseInt(params.id) === 1) {
     return redirect('/blog/');
   }
   return true;
-}
+};
+
+export default blogIndexRedirect;
