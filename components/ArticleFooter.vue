9946cc6943d6dae8dc33e9560a66a441d8c36ae4
Joshua Steele <joshukraine@gmail.com>
2021-07-30 16:43:06 +0300

    Update TGBTG svg

--- components/ArticleFooter.vue
+++ components/ArticleFooter.vue
@@ -2,11 +2,11 @@
   <section class="my-8 md:my-12">
     <div class="max-w-lg mx-auto px-6">
       <img
-        src="~/assets/svg/to-god-be-the-glory.svg"
+        src="~/assets/svg/to-god-be-the-glory-2021.svg"
         alt="To God be the Glory"
       />
     </div>
-    <hr class="border-t-4 my-8" />
+    <hr class="border-t-4 my-10" />
     <div class="max-w-lg mx-auto">
       <h3 class="mt-0 text-center text-2xl md:text-3xl">
         Like what you're reading?<br />Let's keep in touch!
