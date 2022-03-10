2f6627920d2a86779ec2d84d380cdf525b9f47e6
Joshua Steele <joshukraine@gmail.com>
2022-03-10 13:59:18 +0100

    Add podcast links to nav, footer

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,19 +1,7 @@
 <template>
   <div>
     <header
-      class="
-        fixed
-        z-50
-        flex
-        items-center
-        justify-between
-        w-full
-        h-16
-        px-4
-        bg-white
-        border-b border-gray-200
-        lg:px-6
-      "
+      class="fixed z-50 flex items-center justify-between w-full h-16 px-4 bg-white border-b border-gray-200 lg:px-6"
       :class="{ 'open-nav': navOpen }"
     >
       <nuxt-link class="block" to="/">
@@ -27,6 +15,14 @@
         :class="{ 'reveal-nav': navOpen }"
       >
         <div class="nav-link-wrapper">
+          <a
+            class="nav-link"
+            :class="{ 'nav-link-fadein nav-link-1': navOpen }"
+            href="https://journeytoukraine.buzzsprout.com/"
+            target="_blank"
+          >
+            Podcast
+          </a>
           <nuxt-link
             class="nav-link"
             :class="{ 'nav-link-fadein nav-link-1': navOpen }"
