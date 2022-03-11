3bf95b6029733febcc2aa466f8192b8a7668ee05
Joshua Steele <joshukraine@gmail.com>
2022-03-11 12:11:33 +0100

    Add podcast page

---
+++ content/pages/podcast.md
@@ -0,0 +1,23 @@
+<figure class="my-6 md:my-10">
+  <client-only>
+    <cld-image
+      public-id="OFReport/assets/logo_horiz_centered_jp0r8k.png"
+      loading="lazy"
+      alt="Journey to Ukraine"
+    >
+      <cld-transformation height="224" width="640" crop="fill" fetchFormat="auto" />
+    </cld-image>
+  </client-only>
+</figure>
+
+<article-callout content="A podcast by Joshua and Kelsie Steele" mt="0" />
+
+In March of 2022, our family was forced to evacuate from Ukraine. We have served as missionaries in L’viv for over 20 years, and now, like so many others, we find ourselves suddenly displaced from our home, our church, and our precious Ukrainian friends. But despite the shock of evacuation, God is opening doors and leading us step by step down this new path. Our purpose is to bless and minister to Ukrainians affected by the war. Come with us as we share our stories: striving to serve God, bless people, and praying that someday soon this journey will lead us back to our beloved Ukraine.
+
+<article-spacer />
+
+<article-button text="Subscribe now!" path="https://journeytoukraine.buzzsprout.com/" :outline="true" :external="true" margin="b" :center="true" />
+
+<article-spacer />
+
+<div id='buzzsprout-large-player'></div><script type='text/javascript' charset='utf-8' src='https://www.buzzsprout.com/1953515.js?container_id=buzzsprout-large-player&player=large'></script>
---
+++ pages/podcast.vue
@@ -0,0 +1,52 @@
+<template>
+  <article class="pb-8 md:pb-12">
+    <section class="container max-w-2xl mx-auto">
+      <DynamicMarkdown dir="pages" slug="podcast" />
+    </section>
+  </article>
+</template>
+
+<script>
+import DynamicMarkdown from '~/components/DynamicMarkdown.vue';
+
+export default {
+  components: {
+    DynamicMarkdown,
+  },
+  data() {
+    return {
+      description:
+        'Documenting our journey to minister to displaced Ukrainians in Eastern Europe.',
+      image:
+        'https://res.cloudinary.com/dnkvsijzu/image/upload/c_scale,f_auto,h_630,q_auto:best,w_1200/v1646995840/OFReport/assets/jtu_logo_horiz_12-6_lm3zil.png',
+      title: 'Journey to Ukraine',
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.description },
+        { hid: 'og:image', property: 'og:image', content: this.image },
+        { hid: 'og:title', property: 'og:title', content: this.title },
+        { hid: 'og:image:width', property: 'og:image:width', content: '1200' },
+        { hid: 'og:image:height', property: 'og:image:height', content: '630' },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.description,
+        },
+        { hid: 'twitter:title', name: 'twitter:title', content: this.title },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.description,
+        },
+        { hid: 'twitter:image', name: 'twitter:image', content: this.image },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
