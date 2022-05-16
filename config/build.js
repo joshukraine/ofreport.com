b8512c86666eb8c2f7d94156aca74c7a96f33373
Joshua Steele <joshukraine@gmail.com>
2022-05-16 12:56:34 +0200

    Don't convert emails to links

--- config/build.js
+++ config/build.js
@@ -1,7 +1,15 @@
 import Mode from 'frontmatter-markdown-loader/mode';
-import markdownIt from 'markdown-it';
+import MarkdownIt from 'markdown-it';
 import mila from 'markdown-it-link-attributes';
 import path from 'path';
+
+const md = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+});
+
+md.linkify.set({ fuzzyEmail: false }); // disables converting email to link
 
 export default {
   extractCSS: true,
@@ -14,11 +22,7 @@ export default {
       loader: 'frontmatter-markdown-loader',
       options: {
         mode: [Mode.VUE_COMPONENT],
-        markdownIt: markdownIt({
-          html: true,
-          linkify: true,
-          typographer: true,
-        }).use(mila, {
+        markdownIt: md.use(mila, {
           pattern: /^https?:\/\//,
           attrs: {
             target: '_blank',
